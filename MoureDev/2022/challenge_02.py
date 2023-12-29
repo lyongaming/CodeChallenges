97549c1f85a7c31474f9254c72dd47dfe6a2c8fb
lyongaming <owlsmouth@gmail.com>
2023-12-29 08:19:25 -0400

    feat(MoureDev): :triangular_flag_on_post: solved fibonacci challenge
    In my opinion, the solution looks pretty

---
+++ MoureDev/2022/challenge_02.py
@@ -0,0 +1,18 @@
+"""
+    LA SUCESION DE FIBONACCI
+    Escribe un programa que imprima los 50 primeros numeros de la sucesion 
+    de Fibonacci empezando en 0.
+    - La serie de Fibonacci se compone por una sucesion de números en 
+    la que el siguiente siempre es la suma de los anteriores.
+"""
+
+x0, x1 = 0, 1 
+next = 0
+for i in range(51):
+    if(i == 0 or i == 1):
+        print(i)
+    else:
+        next = x0 + x1
+        print(next)
+        x0 = x1
+        x1 = next
